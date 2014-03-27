1246c47d2cce34a97bc5e3e92f3ea735466d2336
Thomas Klausner <domm@plix.at>
2014-03-27 10:21:09 +0100

    no need to explictly list RouteBuilders in test

--- t/App/lib/Example.pm
+++ t/App/lib/Example.pm
@@ -1,14 +1,13 @@
 package Example;
 use OX;
 use ExampleController;
-use OX::RouteBuilder::REST;
 
 has thing => (
     is  => 'ro',
     isa => 'ExampleController',
 );
 
-router [map {'OX::RouteBuilder::'.$_} qw(REST ControllerAction)] => as {
+router as {
     route '/thing'     => 'REST.thing.root';
     route '/thing/:id' => 'REST.thing.item';
     route '/hase' => 'thing.hase';
